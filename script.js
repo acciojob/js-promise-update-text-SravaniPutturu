cd2e410ce3629032d49007e8dcca3b0555206709
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-24 22:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+var output = document.getElementById('output');
+new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		output.innertText = "Hello, world!";
+	},1000);
+})
